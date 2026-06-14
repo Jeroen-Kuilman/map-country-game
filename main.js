7b22dc204480a882d0db7132711c244b4f7a979c
Jeroen Kuilman <jeroenkuilman.webdesign@gmail.com>
2026-06-14 20:19:48 +0200

    style: changed the map style to CartoDB. Styled search-list

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-// temporary eventlistener to toggle the search list
+// temporary eventlistener to toggle the search list //// REMOVE WHEN FINISHED
 document
   .querySelector("#country-search")
   .addEventListener("click", function () {
@@ -16,8 +16,10 @@ const randomLat = Math.floor(Math.random() * 180 - 90);
 const randomLng = Math.floor(Math.random() * 360 - 180);
 
 const map = L.map("map").setView([randomLat, randomLng], 5);
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+L.tileLayer(
+  "https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png",
+  {
+    maxZoom: 19,
+    attribution: "&copy; OpenStreetMap &copy; CartoDB",
+  },
+).addTo(map);
